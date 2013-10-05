17e30f2abdb49760513deb7115950d1c3a1ded22
Geoffroy Aubry <gaubry@hi-media.com>
2013-10-05 17:46:20 +0200

    Remove out-of-date files.

--- scripts/php/template.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo 'Parameters: ' . implode('|', $argv) . "\n";
-echo 'WARNING alert!' . "\n";
-echo '...' . "\n";
-throw new Exception('arghh');
--- scripts/shell/template.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Parameters: $@"
-echo 'WARNING alert!'
-echo '...'
-#echo argh >&2
-TEST=`ls not_exists`
